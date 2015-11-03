e3d171dc1275399b563ede0d640d9eff67296d61
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2015-11-03 11:55:46 +0100

    include fix

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
+#include <cstring>
 
 using namespace std;
 
